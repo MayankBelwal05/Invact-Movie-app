7fe131dec05c18a72a9105ce6d64a3f145dfffe6
MayankBelwal05 <mbelwal05@gmail.com>
2024-06-21 16:59:39 +0530

    module updated

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,6 @@ app.get('/check', (req, res) => {
   
 app.use('/api/movies', movieRoutes);
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
